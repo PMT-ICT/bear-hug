476e3a1b2c96611252aafc9f3d92831e0b02c25f
Jorard <jorard@pm.me>
2021-04-29 11:35:34 +0200

    fourth

--- webpack/base.js
+++ webpack/base.js
@@ -10,7 +10,7 @@ module.exports = {
     rules: [
       {
         test: /\.js$/,
-        exclude: /node_modules/,
+        exclude: /node_modules\/(?!embrace-wabbits$)/,
         use: {
           loader: "babel-loader"
         }
