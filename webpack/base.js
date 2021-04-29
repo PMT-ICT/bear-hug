0b57337c56212c743cb29c2d7a07439b776e88fc
Jorard <jorard@pm.me>
2021-04-29 11:32:07 +0200

    third

--- webpack/base.js
+++ webpack/base.js
@@ -34,7 +34,7 @@ module.exports = {
       WEBGL_RENDERER: JSON.stringify(true)
     }),
     new HtmlWebpackPlugin({
-      template: "./index.html"
+      template: "node_modules/embrace_wabbits/index.html"
     })
   ]
 };
