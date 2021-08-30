8efa1480aca058e4f84d17d879b8e06c7ef8b1db
Jorard <jorard@pm.me>
2021-08-30 12:06:09 +0200

    tweak

--- webpack/base.js
+++ webpack/base.js
@@ -13,7 +13,7 @@ module.exports = {
     headers: {
       "Access-Control-Allow-Origin": "*",
       "Access-Control-Allow-Methods": "GET, POST, PUT, DELETE, PATCH, OPTIONS",
-      "Access-Control-Allow-Headers": "Content-Type",
+      "Access-Control-Allow-Headers": "X-Requested-With, Content-Type, Authorization",
       "Access-Control-Expose-Headers": "*"
     }
   },
