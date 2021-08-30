cb590380691e8200cf47c640b4c711749500cb95
Jorard <jorard@pm.me>
2021-08-30 11:53:48 +0200

    cors be gone

--- webpack/base.js
+++ webpack/base.js
@@ -7,7 +7,11 @@ module.exports = {
   mode: "development",
   devtool: "eval-source-map",
   devServer: {
-    firewall: false
+    headers: {
+      "Access-Control-Allow-Origin": "*",
+      "Access-Control-Allow-Methods": "GET, POST, PUT, DELETE, PATCH, OPTIONS",
+      "Access-Control-Allow-Headers": "X-Requested-With, content-type, Authorization"
+    }
   },
   module: {
     rules: [
