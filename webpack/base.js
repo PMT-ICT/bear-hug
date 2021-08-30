7ea6ea030ff7128471d9ddba24ee4f7d2d0e1f70
Jorard <jorard@pm.me>
2021-08-30 12:04:55 +0200

    test

--- webpack/base.js
+++ webpack/base.js
@@ -9,11 +9,12 @@ module.exports = {
   devtool: "eval-source-map",
   devServer: {
     host: "0.0.0.0",
-    
+
     headers: {
       "Access-Control-Allow-Origin": "*",
       "Access-Control-Allow-Methods": "GET, POST, PUT, DELETE, PATCH, OPTIONS",
-      "Access-Control-Allow-Headers": "X-Requested-With, content-type, Authorization"
+      "Access-Control-Allow-Headers": "Content-Type",
+      "Access-Control-Expose-Headers": "*"
     }
   },
   module: {
